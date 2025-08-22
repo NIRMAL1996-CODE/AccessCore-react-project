3902ae9d3c4bcf85ec3b47517107be6a81efa2e9
Nirmal Jaiswal <nirmaljaiswal888@gmail.com>
2025-08-22 17:04:48 +0530

    add employees & admin data with localStorage setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Login from './components/Auth/Login';
 import EmployeeDashboard from './components/Dashboard/EmployeeDashboard';
 import AdminDashboard from './components/Dashboard/AdminDashboard';
+import { setLocalStorage } from './utils/LocalStorage';
 
 
 
 const App = () => {
+
   return (
     <div>
       {/* <Login/> */}
